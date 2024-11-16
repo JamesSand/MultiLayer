18a47a8be13214d3f3d53596de89b5f2564d2145
JamesSand <shazz20@mails.tsinghua.edu.cn>
2024-11-16 20:41:09 +0800

    rm useless code

--- kernel_approx.py
+++ kernel_approx.py
@@ -3,37 +3,12 @@ from torch import nn
 import math
 import random
 import numpy as np
+import time
 
 def relative_error(x, y, p='fro', dim=None, keepdim=False, out=None, dtype=None):
     error = torch.norm(x-y, p=p, dim=dim, keepdim=keepdim, out=out, dtype=dtype)
     denorm =  1 / torch.norm(x+y, p=p, dim=dim, keepdim=keepdim, out=out, dtype=dtype)
     return error * denorm
-
-
-# Q K.T = U_Q @ S_Q @ V_Q.T @ (U_K @ S_K @ V_K.T).T, Q, K \in R^{n \times d}
-# Q K.T = U_Q @ S_Q @ V_Q.T @ V_K @ (U_K @ S_K).T
-# get SVD([K, v]) easily from SVD(K); K \in \R^{n \times d} o(n) time
-# incremental SVD / online SVD
-def sketch_reform(key_states):
-    # run approx svd solver for tall matrix
-    # see detail in https://pytorch.org/docs/stable/generated/torch.linalg.svd.html#torch.linalg.svd
-    input_dtype = key_states.dtype
-    b, h, n, d = key_states.shape
-    if n > d: 
-        full_matrices = False
-    else:
-        full_matrices = True
-    if input_dtype == torch.float16:
-        key_states = key_states.to(torch.float32)
-    kU, kS, kVh = torch.linalg.svd(key_states, full_matrices=full_matrices, driver='gesvda') # solver: None, gesvd, gesvdj, gesvda
-    if torch.max(torch.abs(kU)) + torch.max(torch.abs(kVh)) > 60000: # if fail roll back to general svd solver
-        kU, kS, kVh = torch.linalg.svd(key_states, full_matrices=full_matrices, driver='gesvd')
-    
-    sketch = kVh.transpose(2, 3)
-    if input_dtype == torch.float16:
-        sketch = sketch.to(torch.float16)
-        kS = kS.to(torch.float16)
-    return sketch, kS
 
 CHAR_LIST = 'abcdefghijklmnopqrstuvw'
 # TODO: accelerate below
@@ -54,9 +29,6 @@ def order_approx(x, order=1):
 
     # this is really fancy
     x_order = eval(torch_command)
-
-    # breakpoint()
-    # print()
 
     return x_order * denorm # (q.T k)^d / d!
 
@@ -85,46 +57,16 @@ def poly_approx(q, k, order_list=[]):
             denorm = (1 / math.factorial(i+1)) ** 0.5
             q_list.append((q[:,:,:,:r] ** (i+1)) * denorm)
             k_list.append((k[:,:,:,:r] ** (i+1)) * denorm)
-    
-    # for q_item in q_list:
-    #     print(q_item.shape)
-
-    # breakpoint()
-    # print()
 
     q_approx = torch.cat(q_list, dim=-1) 
     k_approx = torch.cat(k_list, dim=-1) 
-
-    # breakpoint()
-    # print()
-
     return q_approx, k_approx
-
-def exp_approx_error(q, k, order_list=[]):
-    attn_weights = torch.matmul(q, k.transpose(2, 3)) 
-    attn_weights = torch.exp(attn_weights)
-
-    q_approx, k_approx = poly_approx(q, k, order_list=order_list)
-    attn_weights_approx = torch.matmul(q_approx, k_approx.transpose(2, 3))
-
-    # breakpoint()
-    # print()
-
-    return relative_error(attn_weights, attn_weights_approx, p='fro', dim=(-1,-2))
 
 def softmax_approx_error(q, k, order_list=[]):
     attn_weights = torch.matmul(q, k.transpose(2, 3)) 
     attn_weights = nn.functional.softmax(attn_weights, dim=-1)
 
     q_approx, k_approx = poly_approx(q, k, order_list=order_list)
-
-    # print(order_list)
-    # print(q.shape)
-    # print(k.shape)
-    # print(q_approx.shape)
-    # print(k_approx.shape)
-
-    # breakpoint()
 
     attn_weights_approx = torch.matmul(q_approx, k_approx.transpose(2, 3))
     
@@ -153,23 +95,9 @@ if __name__ == "__main__":
         n = 10 * 2**d
         # q = torch.randn(b, h, n, d, dtype=dtype).cuda() / d ** 0.5
         # k = torch.randn(b, h, n, d, dtype=dtype).cuda() / d ** 0.5
+        # v = torch.randn(b, h, n, d, dtype=dtype).cuda()
         q = torch.randn(b, h, n, d, dtype=dtype) / d ** 0.5
         k = torch.randn(b, h, n, d, dtype=dtype) / d ** 0.5
+        v = torch.randn(b, h, n, d, dtype=dtype) 
         error_0 = softmax_approx_error(q, k, order_list=[d,d])
         print(n, error_0)
-    # error_1 = softmax_approx_error(q, k, order_list=[32])
-    # error_2 = softmax_approx_error(q, k, order_list=[32,12,5])
-    # error_3 = softmax_approx_error(q, k, order_list=[32,32,12,4,3,2,2,2,2,2])
-    # print(error_0)
-    # print(error_1)
-    # print(error_2)
-    # print(error_3)
-    # print()
-    # print(error_0 - error_1)
-    # print(error_1 - error_2)
-    # print(error_2 - error_3)
-    # query_states, key_states, sketch = sketch_reform(q, k)
-    # print(relative_error(k @ kVh.transpose(2, 3), key_states))
-    # print(sketch.shape)
-    # print(q.shape)
-    # print(query_states.shape)
